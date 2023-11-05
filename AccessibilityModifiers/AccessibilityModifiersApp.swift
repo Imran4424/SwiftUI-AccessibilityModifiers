e44bc4a4821f2b16fb1804beb07da96a62a926d5
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-05 14:02:32 +0600

    project init

---
+++ AccessibilityModifiers/AccessibilityModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityModifiersApp.swift
+//  AccessibilityModifiers
+//
+//  Created by Shah Md Imran Hossain on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
