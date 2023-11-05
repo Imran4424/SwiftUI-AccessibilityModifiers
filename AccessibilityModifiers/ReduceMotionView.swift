d8b3a7a9d529e65ca96a5d82b1b48881086f22a1
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-05 15:51:49 +0600

    Optimal code of reduce animation

--- AccessibilityModifiers/ReduceMotionView.swift
+++ AccessibilityModifiers/ReduceMotionView.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+// for reducing code duplication
+func withOptionalAnimation<Result> (animation: Animation? = .default, body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+}
+
 struct ReduceMotionView: View {
     @Environment(\.accessibilityReduceMotion) var reduceMotion
     @State private var scale = 1.0
@@ -15,14 +24,21 @@ struct ReduceMotionView: View {
         Text("Reduce Motion")
             .scaleEffect(scale)
             .onTapGesture {
-                if reduceMotion {
+                // for reducing code duplication
+                withOptionalAnimation {
                     scale *= 1.5
-                } else {
-                    withAnimation {
-                        scale *= 1.5
-                    }
                 }
             }
+        // this is manual
+//            .onTapGesture {
+//                if reduceMotion {
+//                    scale *= 1.5
+//                } else {
+//                    withAnimation {
+//                        scale *= 1.5
+//                    }
+//                }
+//            }
     }
 }
 
